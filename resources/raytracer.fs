1dee264fed228548236cb228200615ccf1b420f6
Ben Kollar <benjkollar03@gmail.com>
2025-01-27 01:57:40 +0000

    moller-trumbore RT shader

--- resources/raytracer.fs
+++ resources/raytracer.fs
@@ -2,8 +2,6 @@
 in vec3 fPos; //screen coordinates
 
 // uniform mat4 u_transform;
-
-uint vtxSSBO_size;
 uint idxSSBO_size;
 
 out vec4 fColor;
@@ -12,29 +10,31 @@ layout (binding = 2, std430) readonly buffer vtxSSBO{
     vec3 vtxData[]; //consider using vec4 for alignment
 };
 
-/*
-layout (binding = 3, std430) readonly buffer idxSSBO{
+
+layout (binding = 2, std430) readonly buffer idxSSBO{
     uint idxData[];
 };
-*/
 
-bool side(vec3 a,vec3 b,vec3 x){
-    vec2 ax = {x.x-a.x,x.y-a.y};
-    vec2 xb = {b.x-x.x,b.y-x.y};
 
-    return (ax.x*xb.y)-(xb.x*ax.y) > 0;
-    //one liner
-    //return dot(cross(b-a,x-a),vec3(1,1,1)) > 0; //i wonder if this method would be faster, though if vectors are close to parralel to the arbritrary chosen vec then might be issues, btu choose it to be smth parralel to the ray
-}
+//implementation of moller-trumbore.
+//u and v are barycentric coordinates such that intersection point = a1 + u.a2 + v.a3
+bool rayIntersectsTriangle(vec3 rayOrigin, vec3 rayDir, vec3 a1,vec3 a2, vec3 a3,out u, out v){
+    
+    vec3 a1a3 = a3-a1, a1a2=a2-a1, tNorm = cross(a1a2,a1a3);
+    //early return if ray parralel to triangle
+    if(dot(tNorm,rayDir)==0) return false;
 
-bool in_triangle(vec3 a,vec3 b,vec3 c, vec3 x){
-    bool ab_s = side(a,b,x);
-    bool ac_s = side(a,c,x);
-    if(ab_s==ac_s) return false; //not inside cone of ab / ac
+    //t = ..
+    float detleq=determinant(mat3(rayDir,a1a2,a1a3)); //determinant of the system of linear equations
 
-    //side of vec BC (direction matters, not CB)= side of AB
-    return side(b,c,x)==ab_s;
+    //cramer's rule
+    float t = determinant(mat3(-a1,a1a2,a1a3))/detleq;
+    if(t<0) return false; //intersects behind the camera
 
+    float u = determinant(mat3(rayDir,-a1,a1a3))/detleq;
+    float v = determinant(mat3(rayDir,a1a2,-a1))/detleq;
+
+    return true;
 }
 
 void main(){
@@ -44,11 +44,12 @@ void main(){
         //calculate ray equation (not doing reflections yet)
 
         //find intersections
-        vec3 a = vtxData[0];
-        vec3 b = vtxData[1];
-        vec3 c = vtxData[2];
+        for(uint i=0; i<idxSSBO_size;){
+            vec3 a1 = vtxData[idxData[i++]], a2 = vtxData[idxData[i++]], a3=vtxData[idxData[i++]];
+            
+            //atm just one ray from the camera
 
-        fColor = in_triangle(a,b,c,fPos) ? vec4(0.0,1.0,0.0,1.0) :  vec4(1.0,0.0,0.0,1.0); //green in, red out
-        //fColor = fPos.x<0.75 || fPos.y<0.75? vec4(0.0,1.0,0.0,1.0) :  vec4(1.0,0.0,0.0,1.0); 
+            fColor = rayIntersectsTriangle(vec3(0,0,0),vec3(0,0,1),a1,a2,a3) ?  vec4(0.0,1.0,0.0,1.0) :  vec4(1.0,0.0,0.0,1.0); //green in, red out
+        }
     }
 }
